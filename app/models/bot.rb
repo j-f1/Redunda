7607162cdb4a4117fe0279bbb12531554315a9a4
Jed Fox <git@twopointzero.us>
2017-06-05 14:39:25 -0400

    Yell at people with redunda open when a bot dies

--- app/models/bot.rb
+++ app/models/bot.rb
@@ -32,4 +32,18 @@ class Bot < ApplicationRecord
     end
     return result
   end
+  
+  def self.check_bot_status
+    Bot.each do |bot|
+      next unless bot.instances.order_by('last_ping DESC').first.last_ping < 3.minutes.ago
+      bot.instance.each do |instance|
+        ActionCable.server.broadcast "notifications:#{instance.owner.id}", {
+          title: "Bot #{bot.name} is dead!!1",
+          opts: {
+            body: "You own the #{instance.name} instance. Fix the problem!"
+          }
+        }
+      end
+    end
+  end
 end
